3a696ced1052c08e9772345807f6159d00482a37
Falme Streamless <falme_shyness@hotmail.com>
2017-12-17 23:22:08 -0200

    Atualizado Sources

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
 var game;
 
-var possibleSymbol = ["0","1","2","3","4","5","6","7","8","9"];
 
-var possiblePieces = {
-	"list" : [
-		[possibleSymbol[1]],
-		[possibleSymbol[2]],
-		[possibleSymbol[3]],
-		[possibleSymbol[4]],
-		[possibleSymbol[5]],
-		[possibleSymbol[6]],
-		[possibleSymbol[7]],
-		[possibleSymbol[8]],
-		[possibleSymbol[9]],
-		[possibleSymbol[1],possibleSymbol[0]],
-		[possibleSymbol[1],possibleSymbol[1]],
-		[possibleSymbol[1],possibleSymbol[2]],
-		[possibleSymbol[1],possibleSymbol[3]],
-		[possibleSymbol[1],possibleSymbol[4]],
-		[possibleSymbol[1],possibleSymbol[5]],
-		[possibleSymbol[1],possibleSymbol[6]],
-		[possibleSymbol[1],possibleSymbol[7]],
-		[possibleSymbol[1],possibleSymbol[8]],
-		[possibleSymbol[1],possibleSymbol[9]],
-		[possibleSymbol[2],possibleSymbol[0]]
-	]
-};
 
 /*class PieceCreator{
 	constructor(n){
